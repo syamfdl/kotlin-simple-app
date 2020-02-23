2e3b9e8fdedf0d65b5a25668bbc7875dcabe5a81
syamfdl <syamfdl.lvlup@gmail.com>
2020-02-24 01:20:32 +0700

    calculate-shape

--- app/src/main/java/me/syamfdl/simpleapp/MainActivity.kt
+++ app/src/main/java/me/syamfdl/simpleapp/MainActivity.kt
@@ -9,9 +9,11 @@ import me.syamfdl.simpleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navController = this.findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
--- app/src/main/java/me/syamfdl/simpleapp/PersegiFragment.kt
+++ app/src/main/java/me/syamfdl/simpleapp/PersegiFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.fragment_persegi.*
 import me.syamfdl.simpleapp.databinding.FragmentPersegiBinding
 
 /**
@@ -19,6 +20,30 @@ class PersegiFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentPersegiBinding>(inflater, R.layout.fragment_persegi, container, false)
+
+
+
+        binding.tvLuas.setVisibility(View.GONE)
+        binding.tvKeliling.setVisibility(View.GONE)
+        binding.btnShare.setVisibility(View.GONE)
+
+        binding.btnHitung.setOnClickListener {
+
+            var panjangPersegi = etPanjang.text.toString().toDouble()
+            var lebarPersegi = etLebar.text.toString().toDouble()
+
+            var luasPersegi = panjangPersegi * lebarPersegi
+            var kelilingPersegi = 2*(panjangPersegi + lebarPersegi)
+
+            binding.tvLuas.setVisibility(View.VISIBLE)
+            binding.tvKeliling.setVisibility(View.VISIBLE)
+            binding.btnShare.setVisibility(View.VISIBLE)
+
+            tvLuas.text = luasPersegi.toString()
+
+            tvKeliling.text = kelilingPersegi.toString()
+        }
+
         // Inflate the layout for this fragment
         return binding.root
     }
